ee3b4afd771e341f4237c8ed52635107cbb7ab62
BlueIceGamerGirL <ameliebfgamer@gmail.com>
2026-09-16 13:44:45 -0400

    Début classe equipment

--- RPGProject/src/Equipment.java
+++ RPGProject/src/Equipment.java
@@ -1,7 +1,19 @@
 public class Equipment{
     private String equipment;
+    private String armes;
+    private String armures;
 
-    public Equipment(String equipment){
+    public Equipment(String equipment, String armes, String armures){
         this.equipment = equipment;
+        this.armes = armes;
+        this.armures = armures;
+    }
+
+    public Armes(String armes){
+
+    }
+
+    public Armures(String armures) {
+
     }
 }
