2b655571c8de66626414e94d8b9cdd11a46dee80
BlueIceGamerGirL <ameliebfgamer@gmail.com>
2026-09-17 12:45:10 -0400

    Commencé Weapons et Armors

---
+++ src/Armors.java
@@ -0,0 +1,11 @@
+public class Armors {
+    private String armorProtection;
+    private String armorClass;
+
+    public Armors(String protection, String armorClass){
+        this.armorProtection = armorProtection;
+        this.armorClass = armorClass;
+
+        String[] armors = {"No armor", "Leather armor", "Chainmail armor"};
+    }
+}
---
+++ src/Weapons.java
@@ -0,0 +1,13 @@
+public class Weapons {
+    private String weaponName;
+    private String weaponDamage;
+    private String weaponStat;
+
+    public Weapons(String weaponName, String weaponDamage, String weaponStat) {
+        this.weaponName = weaponName;
+        this.weaponDamage = weaponDamage;
+        this.weaponStat = weaponStat;
+
+        String[] weapons = {"Long Sword", "Axe", "Mace", "Dagger", "Bow", "Stick"};
+    }
+}
