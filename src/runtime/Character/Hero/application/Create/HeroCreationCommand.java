eab9b79d6e5da13b82c6083703ec40f0a4a68bae
LeslyAmanda <2559512@cegep.mv>
2026-09-24 13:41:19 -0400

    Ajourt des caractéristiques du héros

--- src/runtime/Character/Hero/application/Create/HeroCreationCommand.java
+++ src/runtime/Character/Hero/application/Create/HeroCreationCommand.java
@@ -1,5 +1,14 @@
 package runtime.Character.Hero.application.Create;
 
-public record HeroCreationCommand(String name) {
+public record HeroCreationCommand(
+        String name,
+        String spiecies,
+        String type,
+        int strenght,
+        int dexterity,
+        int constitution,
+        int intelligence,
+        int wisdom,
+        int charisma) {
 
 }
